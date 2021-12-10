62d42d2c11624d1b227c345ae800fe52ad703085
Ananda-git <ananda.perl.trainer@gmail.com>
2021-12-10 11:05:03 +0530

    webapp creation start

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,16 @@
+import pytest
+
+class NotInRange(Exception):
+    def __init__(self, message="Value not in range "):
+        self.message = message
+        super().__init__(self.message)
+
+#def test_exceptions() :
+#    a=1
+#    with pytest.raises(ValueError):
+#        if a not in range(10,20):
+#            raise NotInRange
+
 def test_generic() :
     a = 2
     b = 2
